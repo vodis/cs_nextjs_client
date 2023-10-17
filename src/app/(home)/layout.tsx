1f4d527aa7b31baaae8492e22160d9a5cc7cd882
vodis <i402178791@gmail.com>
2023-10-17 11:06:31 +0100

    feat/CS-10 - Mobile layout
    - use dvh calculation

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -6,7 +6,7 @@ import Panels from "@src/components/Panels";
 const HomeLayout: React.FC<PropsWithChildren> = ({ children }) => {
     const [play, setPlay] = useState(true);
     return (
-        <div className="w-full h-[calc(100vh-8rem)] max-auto">
+        <div className="w-full h-[calc(100vh-8rem)] min-h-[calc(100dvh-8rem)] max-auto">
             <div className="hidden lg:block">
                 <Panels play={play} text={"future".toUpperCase()} />
             </div>
