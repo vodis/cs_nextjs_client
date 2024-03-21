73bd2d558dc50aa36e4e0069d62823d24ad5825a
vodis <i402178791@gmail.com>
2024-03-21 00:38:45 +0000

    feat/CS-22 - Add content to home page
    - fix InfoBox interface

--- src/components/InfoBox/types.ts
+++ src/components/InfoBox/types.ts
@@ -1,8 +1,9 @@
 import {ReactNode} from "react";
+import {StaticImport} from "next/dist/shared/lib/get-img-props";
 
 export interface IInfoBox {
     children: ReactNode | undefined;
     title: string;
-    img?: string;
+    img?: string | StaticImport;
     imgPosition?: 'left' | 'right';
 }
