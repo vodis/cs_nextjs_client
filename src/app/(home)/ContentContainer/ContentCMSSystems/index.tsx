f34d68e4848842d6d3d5d3ff65bdc6d737aa6755
vodis <i402178791@gmail.com>
2024-03-22 20:05:44 +0000

    feat/CS-26 - Update translations
    - use translation keys instead static text on home content

--- src/app/(home)/ContentContainer/ContentCMSSystems/index.tsx
+++ src/app/(home)/ContentContainer/ContentCMSSystems/index.tsx
@@ -1,13 +1,15 @@
 import {InfoBox} from "@src/components/InfoBox";
+import Translate from "@vodis/ui-kit/i18n/Translate";
+import React from "react";
 
 const ContentCMSSystems = () => {
     return (
         <InfoBox img="/images/home/home_cms_systems_card.jpg" title="CMS Systems">
             <section className="flex flex-col gap-2">
-                <h2 className="mt-10 mb-5 text-xl tracking-wide uppercase text-orange">Custom CMS Systems</h2>
-                <p>Transform your digital presence with CraftScript's bespoke Content Management System (CMS) solutions. As pioneers in the field of web3 technologies, CraftScript specializes in developing modular and scalable CMS systems tailored to meet the unique needs of your business.</p>
-                <p>With CraftScript's CMS solutions, you'll have full control over your digital content and effortlessly manage your online assets. Our user-friendly interfaces and robust features empower you to create, publish, and update content with ease, driving engagement and enhancing your brand's online visibility.</p>
-                <p>Experience the next generation of content management with CraftScript's innovative CMS systems. Revolutionize your digital strategy and stay ahead of the competition in today's dynamic digital landscape.</p>
+                <h2 className="mt-10 mb-5 text-xl tracking-wide uppercase text-orange"><Translate translationKey="Texts.content-content-cms-systems-title" /></h2>
+                <p><Translate translationKey="Texts.content-content-cms-systems-paragraph-1" /></p>
+                <p><Translate translationKey="Texts.content-content-cms-systems-paragraph-2" /></p>
+                <p><Translate translationKey="Texts.content-content-cms-systems-paragraph-3" /></p>
             </section>
         </InfoBox>
     );
